78bdd4e3da13df35932210cc52a573e932f8aba6
Divine Philip <dpkreativ@gmail.com>
2021-05-25 15:48:30 +0100

    create routes for the different CRUD ops

--- routes/tea.js
+++ routes/tea.js
@@ -2,6 +2,22 @@ const express = require("express");
 const router = express.Router();
 const teaController = require("../controllers/tea");
 
+// Post new tea
 router.post("/tea", teaController.newTea);
+
+// Get all tea
+router.get("/tea", teaController.getAllTea);
+
+// Delete all tea
+router.delete("/tea", teaController.deleteAllTea);
+
+// Get specific tea
+router.get("/tea/:name", teaController.getTea);
+
+// Post comment on tea
+router.post("/tea/:name", teaController.teaComment);
+
+// Delete specific tea
+router.delete("/tea/:name", teaController.deleteTea);
 
 module.exports = router;
