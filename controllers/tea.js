96223e34fb4939f8ad9c1645304ea4df32bb8c73
Divine Philip <dpkreativ@gmail.com>
2021-05-25 15:48:11 +0100

    add controllers to handle different CRUD ops

--- controllers/tea.js
+++ controllers/tea.js
@@ -1,5 +1,38 @@
+// Create new tea
 const newTea = (req, res, next) => {
   res.json({ message: "POST new tea" }); // dummy function for now
 };
 
-module.exports = { newTea };
+// Get all tea
+const getAllTea = (req, res, next) => {
+  res.json({ message: "GET all tea" });
+};
+
+// Delete all tea
+const deleteAllTea = (req, res, next) => {
+  res.json({ message: "DELETE all tea" });
+};
+
+// Get specific tea
+const getTea = (req, res, next) => {
+  res.json({ message: "GET specific tea" });
+};
+
+// Add comment for tea
+const teaComment = (req, res, next) => {
+  res.json({ message: "POST tea commment" });
+};
+
+// Delete specific tea
+const deleteTea = (req, res, next) => {
+  res.json({ message: "DELETE specific tea" });
+};
+
+module.exports = {
+  newTea,
+  getAllTea,
+  deleteAllTea,
+  getTea,
+  teaComment,
+  deleteTea,
+};
