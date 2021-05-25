f52b1a14deb26e9ee35577855290a213da53133d
Divine Philip <dpkreativ@gmail.com>
2021-05-25 15:33:25 +0100

    add routes to server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const routes = require("./routes/tea"); // import routes
 const app = express();
 
 app.use(express.json()); // to parse incoming requests with JSON payloads
+app.use("/", routes); // use the routes
 
 const listener = app.listen(process.env.PORT || 5000, () => {
   console.log(`App is listening on port ${listener.address().port}`);
